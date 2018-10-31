1f7503160426a6fa8c9a242d128abf36db812262
Simon Nilsson <sim.f.nilsson@gmail.com>
2018-11-01 00:06:50 +0100

    emacs: add powerline

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,16 +16,18 @@
 (require 'whitespace)
 (require 'linum)
 (require 'vdiff)
-(require 'custom-lisp-functions)
 (require 'python)
 (require 'server)
 (require 'rtags)
+(require 'powerline)
+(require 'custom-lisp-functions)
 
 
 ;; Default settings
 ;; ----------------
 ;; Set default theme
 (load-theme 'paganini t)
+(powerline-default-theme)
 (show-trailing-whitespace)
 ;; General default settings
 (setq-default tab-width 4)
@@ -190,7 +192,7 @@
     ("1e67765ecb4e53df20a96fb708a8601f6d7c8f02edb09d16c838e465ebe7f51b" "65d9573b64ec94844f95e6055fe7a82451215f551c45275ca5b78653d505bc42" "2b6bd2ebad907ee42b3ffefa4831f348e3652ea8245570cdda67f0034f07db93" "7f3ef7724515515443f961ef87fee655750512473b1f5bf890e2dc7e065f240c" "7feeed063855b06836e0262f77f5c6d3f415159a98a9676d549bfeb6c49637c4" default)))
  '(package-selected-packages
    (quote
-    (cmake-ide company-rtags flycheck-rtags rtags company flycheck monokai-theme paganini-theme vdiff-magit vdiff color-theme fill-column-indicator hemisu-theme gruvbox-theme))))
+    (powerline cmake-ide company-rtags flycheck-rtags rtags company flycheck monokai-theme paganini-theme vdiff-magit vdiff color-theme fill-column-indicator hemisu-theme gruvbox-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
