38d8fcc9b1a5dec3b52a672937063355fa74c8ce
Simon Nilsson <sim.f.nilsson@gmail.com>
2019-12-06 00:06:58 +0100

    bash: tmux/screen support for bash depth

--- .profile.d/00_bashdepth.sh
+++ .profile.d/00_bashdepth.sh
@@ -1,3 +1,9 @@
 if [[ -n $PS1 ]]; then
     export _INTERACTIVE_BASH_DEPTH=$((1+${_INTERACTIVE_BASH_DEPTH:-0}))
+    if [[ $TERM =~ screen ]]; then  # both screen and tmux sets TERM=screen
+        if [[ -z $_ALREADY_IN_TMUX ]]; then
+            _INTERACTIVE_BASH_DEPTH=1
+        fi
+        export _ALREADY_IN_TMUX=true
+    fi
 fi
